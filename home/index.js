2afe35450379412f8aa41b84b4dfd440ffcc4099
Helloworld <Teohjyim@gmail.com>
2021-12-09 10:42:25 +1100

    Mobile fix again

--- home/index.js
+++ home/index.js
@@ -1,6 +1,8 @@
 let scrollDownEl = document.getElementById("scrollDown");
 let scrollUpEl = document.getElementById("scrollUp");
 let mainEl = document.getElementById("main");
+
+let discordEl = document.getElementById("discord");
 VANTA.WAVES({
   el: "#vanta-header",
   mouseControls: true,
@@ -26,7 +28,7 @@ document.addEventListener("wheel", e => {
     blocking = true;
     setTimeout(() => { blocking = false; }, 300);
   }
-  if (window.scrollY+e.deltaY < mainEl.offsetTop && Math.sign(e.deltaY) === -1) {
+  if (window.scrollY + e.deltaY < mainEl.offsetTop && Math.sign(e.deltaY) === -1) {
     e.preventDefault();
     window.scroll({ top: 0, left: 0, behavior: 'smooth' });
     blocking = true;
@@ -53,3 +55,24 @@ document.addEventListener("scroll", e => {
     scrollUpEl.style.display = "block";
   }
 });
+let discordMain = `<img src="discord.png" alt="Discord" class="social-icon">Discord`;
+let discordInfo = `My discord is: helloworld#3059`;
+let discordTimer;
+let discordLastDown = 0;
+function changeDiscord() {
+  if (Date.now() - discordLastDown > 1500) return;
+  clearTimeout(discordTimer);
+  if (discordEl.innerHTML === discordMain) {
+    discordEl.innerHTML = "My discord is: helloworld#3059";
+    discordTimer = setTimeout(() => {
+      discordEl.innerHTML = discordMain;
+    }, 10000);
+  }
+  else {
+    discordEl.innerHTML = `<img src="discord.png" alt="Discord" class="social-icon">Discord`;
+  }
+}
+discordEl.addEventListener("mousedown", e => { discordLastDown = Date.now(); });
+discordEl.addEventListener("mouseup", changeDiscord);
+discordEl.addEventListener("touchstart", e => { discordLastDown = Date.now(); })
+discordEl.addEventListener("touchend", changeDiscord)
